abb7e88fc9d24dc5db0b3a5c4bf4362255e3f3bf
kradlus <markraf96@gmail.com>
2022-08-09 12:34:27 -0400

    feat:login endpoint

--- backend/src/app/routers/auth.py
+++ backend/src/app/routers/auth.py
@@ -1,18 +1,25 @@
 from fastapi import APIRouter, status, Body
 from fastapi.responses import JSONResponse
 
-from app.models import RegisterModel
+from app.models import LoginModel, RegisterModel
 
 router:APIRouter = APIRouter(prefix="/auth")
 
 @router.get("/login")
-async def auth_login():
+async def auth_login() -> JSONResponse:
     return JSONResponse(status_code=status.HTTP_200_OK, content={
         "message":"Provide username and password"
     })
 
 @router.post("/register")
-async def auth_login(auth:RegisterModel = Body()):
+async def auth_register(auth:RegisterModel = Body()) -> JSONResponse:
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={
         "message":f"User {auth.username} created!"
+    })
+
+@router.post("/login")
+async def auth_login(auth:LoginModel = Body()) -> JSONResponse:
+    return JSONResponse(status_code=status.HTTP_200_OK, content={
+        "message":f"Logged In!",
+        "token":"" # Implement
     })
