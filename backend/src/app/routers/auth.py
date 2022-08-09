e23375cbf5b4036ea30ef230799f3de0cc56b08b
kradlus <markraf96@gmail.com>
2022-08-09 12:32:32 -0400

    feat: register endpoint

--- backend/src/app/routers/auth.py
+++ backend/src/app/routers/auth.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Body
 from fastapi.responses import JSONResponse
+
+from app.models import RegisterModel
 
 router:APIRouter = APIRouter(prefix="/auth")
 
-@router.get("/")
+@router.get("/login")
 async def auth_login():
     return JSONResponse(status_code=status.HTTP_200_OK, content={
         "message":"Provide username and password"
     })
 
+@router.post("/register")
+async def auth_login(auth:RegisterModel = Body()):
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={
+        "message":f"User {auth.username} created!"
+    })
