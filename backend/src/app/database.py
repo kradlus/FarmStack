7fb3b54db5ff39b0c7e5a60869009a24c00e3ad6
kradlus <markraf96@gmail.com>
2022-08-09 15:36:47 -0400

    feat:auth

--- backend/src/app/database.py
+++ backend/src/app/database.py
@@ -1,15 +1,16 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.models import AuthModel, FindOneModel
+from typing import Dict, Optional
 
 async def get_database_connection() -> AsyncIOMotorClient:
     return AsyncIOMotorClient("mongodb://krad:fakepassword@10.11.0.2/")
 
-async def db_insert_one(obj:AuthModel) -> None:
+async def db_insert_one(obj:AuthModel) -> Dict:
     connection:AsyncIOMotorClient = await get_database_connection()
     db = connection.test
     return db.farm.insert_one(obj.dict())
 
-async def db_find_one(obj:FindOneModel) -> AuthModel:
+async def db_find_one(obj:FindOneModel) -> Optional[Dict]:
     connection:AsyncIOMotorClient = await get_database_connection()
     db = connection.test
     return await db.farm.find_one(obj.dict())
--- backend/src/app/routers/auth.py
+++ backend/src/app/routers/auth.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import HTTPException
 from app.models import LoginModel, RegisterModel, FindOneModel, User
 from app.database import db_insert_one, db_find_one
 from passlib.hash import bcrypt
+from typing import Optional
 
 router:APIRouter = APIRouter(prefix="/auth")
 
@@ -16,8 +17,9 @@ async def auth_register(auth:RegisterModel = Body()) -> JSONResponse:
 
 @router.post("/login")
 async def auth_login(auth:LoginModel = Body()) -> JSONResponse:
-    user:User = User(**await db_find_one(FindOneModel.from_orm(auth)))
-    if not bcrypt.verify(auth.password, user.password):
+    model = await db_find_one(FindOneModel.from_orm(auth))
+    user:Optional[User] = None if not model else User(**model)
+    if not user or not bcrypt.verify(auth.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail={"message":"Invalid username or password"}
