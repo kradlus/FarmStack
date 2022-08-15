3888bf173afd883b2f25d0d1982d6b2a8615951e
kradlus <markraf96@gmail.com>
2022-08-15 09:16:37 -0400

    fix:auth

--- frontend/app/src/router/index.ts
+++ frontend/app/src/router/index.ts
@@ -7,7 +7,6 @@ import httpService from '../api/http'
 const isAuthenticated = async (cookie:string) => {
     const response_status:number = await httpService.apiRoot(cookie).then((resp) => resp.status)
         .catch(error => error.response.status)
-    console.log(response_status)
     return response_status !== undefined && response_status == 200 ? true:false;
 }
 
