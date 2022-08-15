26f4e7e6b2a32e01ed6cff3691785f3da4268340
kradlus <markraf96@gmail.com>
2022-08-15 06:05:55 -0400

    fix:cors

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -9,7 +9,7 @@ def create_app() -> FastAPI:
     app = FastAPI()
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+        allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
